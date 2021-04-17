0c558c7890d9f0dd3616ddd33a052ab124e3fdf4
Lyubomyr <lyubomyr83@mail.ru>
2021-04-17 16:34:44 +0300

    Угадай число

---
+++ guess.py
@@ -0,0 +1,34 @@
+from random import randint
+
+name = input("Как вас зовут?\n")
+
+play = "да"
+
+
+while play == "да":
+    # Попросим ввести число
+    user_number = int(input(f"{name}, до скольки загадать мне число?\n"))
+    # Загадаем число
+    guessed_number = randint(1, user_number)
+    # Количество попыток
+    attempts = 4
+
+    ok = False
+
+
+    print(f"Хорошо, {name}, я загадал число от 1 до {user_number}")
+
+    for i in range(attempts):
+        answer = int(input(f"Попытка {i+1}:\n"))
+        if answer > guessed_number:
+            print("Слишком много!")
+        elif answer < guessed_number:
+            print("Слишком мало!")
+        else:
+            print("Угадал")
+            ok = True
+            break
+    if ok == False:
+        print(f"Я загадал число {guessed_number}")
+        
+    play = input('Еще попытаешь удачу? (да/нет)\n')
